8cb6fb5975fb3053d7729559bac26c9ffbf63e0d
Liz Hennessy <lizhennessy@Lizs-MacBook-Pro.local>
2024-09-17 14:46:07 -0600

    update with iterator comment

--- src/resolvers/Subscription.ts
+++ src/resolvers/Subscription.ts
@@ -5,11 +5,13 @@ export const Subscription: Resolvers = {
   Subscription: {
     listenForMessageInConversation: {
       // @ts-ignore
-      subscribe: async (_, { fromMessageReceivedAt, id }, { pubsub, dataSources }) => {
+      subscribe: async (payload, { fromMessageReceivedAt, id }, { pubsub, dataSources }) => {
+        console.log("others", payload)
           // GOAL: If a cursor `fromMessageReceivedAt` is passed, fetch all messages sent after
           
           // Check whether a timestamp was passed
           const timestampMs = parseInt(fromMessageReceivedAt);
+
          
           // Validate that timestamp is a number, if so retrieve messages sent after that timestamp
           if (!isNaN(timestampMs) && timestampMs > 0) {
@@ -29,58 +31,26 @@ export const Subscription: Resolvers = {
                 console.log("creating a new iterator each time")
                 // The thing we want to do with every new message
                 let iterator = {
-                  [Symbol.asyncIterator]: () => pubsub.asyncIterator(["NEW_MESSAGE_SENT"])
+                   [Symbol.asyncIterator]: () => pubsub.asyncIterator(["NEW_MESSAGE_SENT"])
+                  // [Symbol.asyncIterator]: filtered
                 }
 
                 // The loop that awaits new message events and yields them?
                 for await (const v of iterator) {
-                  console.log("I get called every time there's a new message")
-                  yield v;
+                  // is there a better way that we can filter here? withFilter has a hard time
+                  // @ts-ignore
+                  if (v.conversationId == id) {
+                    yield v;
+                  }
                 }
-              }
+              
             }
           }
 
         // If no timestamp is passed, handle new messages as we normally would
-        return pubsub.asyncIterator(["NEW_MESSAGE_SENT"])
         }
+          return pubsub.asyncIterator(["NEW_MESSAGE_SENT"])
+      }
     }
-    // listenForMessageInConversation: {
-    //   // @ts-ignore
-    //   subscribe: withFilter(
-    //     (_, { fromMessageReceivedAt, id }, { pubsub, dataSources }) => {
-    //       // GOAL: If a cursor `fromMessageReceivedAt` is passed, fetch all messages sent after
-          
-    //       // Check whether a timestamp was passed
-    //       const timestampMs = isNaN(fromMessageReceivedAt) ? 0 : parseInt(fromMessageReceivedAt);
-         
-    //       // Validate that timestamp is a number, if so retrieve messages sent after that timestamp
-    //       if (!isNaN(timestampMs) && timestampMs > 0) {
-    //         const messages = dataSources.db.getMessagesAfterDate(timestampMs, id)
-            
-    //         return {
-    //           async *[Symbol.asyncIterator]() {
-    //             for (let i = 0; i < messages.length; i++) {
-    //               console.log(messages[i])
-    //               yield { newMessages: messages[i]}
-    //             }
-
-    //             let iterator = {
-    //               [Symbol.asyncIterator]: () => pubsub.asyncIterator(["NEW_MESSAGE_SENT"])
-    //             }
-
-    //             for await (const v of iterator) {
-    //               yield v;
-    //             }
-    //           }
-    //         }
-    //       }
-
-
-    //     return pubsub.asyncIterator(["NEW_MESSAGE_SENT"])
-    //     }, (payload, variables) => {
-    //       return payload.conversationId === variables.id
-    //   })
-    // }
   }
 }
